9c66530312384a8582d7c9692d6ebfc64991d78c
Michael <mvasilyev04@gmail.com>
2025-11-13 19:16:13 +0300

    change pr model, add assignedReviewers

--- internal/models/pr.go
+++ internal/models/pr.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type PullRequest struct {
-	PullRequestID   string     `db:"pull_request_id" json:"pull_request_id"`
-	PullRequestName string     `db:"pull_request_name" json:"pull_request_name"`
-	AuthorID        string     `db:"author_id" json:"author_id"`
-	Status          string     `db:"status" json:"status"`
-	NeedMore        bool       `db:"need_more_reviewers" json:"needMoreReviewers"`
-	CreatedAt       time.Time  `db:"created_at" json:"createdAt"`
-	MergedAt        *time.Time `db:"merged_at" json:"mergedAt,omitempty"`
+	PullRequestID     string     `db:"pull_request_id" json:"pull_request_id"`
+	PullRequestName   string     `db:"pull_request_name" json:"pull_request_name"`
+	AuthorID          string     `db:"author_id" json:"author_id"`
+	Status            string     `db:"status" json:"status"`
+	NeedMore          bool       `db:"need_more_reviewers" json:"needMoreReviewers"`
+	AssignedReviewers []string   `json:"assigned_reviewers"`
+	CreatedAt         time.Time  `db:"created_at" json:"createdAt"`
+	MergedAt          *time.Time `db:"merged_at" json:"mergedAt,omitempty"`
 }
 
 type PullRequestShort struct {
