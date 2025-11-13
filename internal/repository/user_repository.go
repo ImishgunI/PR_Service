6665c5807aa4444e595039569eebfe3b9f62341b
Michael <mvasilyev04@gmail.com>
2025-11-13 23:43:53 +0300

    change update query in SetIsActive function

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -17,7 +17,8 @@ func NewUserHandler(db *db.DataBase) *UserRepository {
 
 func (u *UserRepository) SetIsActive(ctx context.Context, userID string, isActive bool) (*models.User, error) {
 	var user models.User
-	err := u.db.Db.QueryRow(ctx, "UPDATE users SET is_active = $2 WHERE user_id = $1 RETURNING user_id, username, team_name, is_active").Scan(&user.UserID, &user.Username, &user.TeamName, &user.Is_Active)
+	err := u.db.Db.QueryRow(ctx, `UPDATE users SET is_active = $2 WHERE user_id = $1
+		RETURNING user_id, username, team_name, is_active`, userID, isActive).Scan(&user.UserID, &user.Username, &user.TeamName, &user.Is_Active)
 	if err != nil {
 		return nil, errors.New("user_not_found")
 	}
