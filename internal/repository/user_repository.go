754478253e549284b55a4d134b08bca9df141c46
Michael <mvasilyev04@gmail.com>
2025-11-13 19:49:39 +0300

    create getRevies

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -23,3 +23,27 @@ func (u *UserRepository) SetIsActive(ctx context.Context, userID string, isActiv
 	}
 	return &user, nil
 }
+
+func (u *UserRepository) GetReview(ctx context.Context, userID string) ([]models.PullRequestShort, error) {
+	rows, err := u.db.Db.Query(ctx, `
+			SELECT pr.pull_request_id, pr.pull_request_name, pr.author_id, pr.status
+			FROM pull_requests pr
+			JOIN pr_reviewers rr ON pr.pull_request_id = rr.pull_request_id
+			WHERE rr.reviewer_id = $1
+		`, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var prs []models.PullRequestShort
+	for rows.Next() {
+		var pr models.PullRequestShort
+		if err := rows.Scan(&pr.PullRequestID, &pr.PullRequestName, &pr.AuthorID, &pr.Status); err != nil {
+			return nil, err
+		}
+		prs = append(prs, pr)
+	}
+
+	return prs, nil
+}
