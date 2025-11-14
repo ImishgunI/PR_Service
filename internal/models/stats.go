cd3e25f2e06d87d4d13bd43c84e04606205b57e9
Michael <mvasilyev04@gmail.com>
2025-11-15 01:29:46 +0300

    create DTO model for pull request stat

--- internal/models/stats.go
+++ internal/models/stats.go
@@ -5,3 +5,9 @@ type ReviewersStats struct {
 	Username      string `json:"username"`
 	AssignedCount int    `json:"assigned_count"`
 }
+
+type PRStats struct {
+	PrID           string `json:"pull_request_id"`
+	PrName         string `json:"pull_request_name"`
+	ReviewersCount int    `json:"reviewers_count"`
+}
