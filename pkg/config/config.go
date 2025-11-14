3766e70ac28d83fe6a2c9a3b6e52bebe66351053
Michael <mvasilyev04@gmail.com>
2025-11-14 23:15:50 +0300

    add error handling for config reading

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,15 +1,20 @@
 package config
 
 import (
+	"PullRequestService/pkg/logger"
+
 	"github.com/spf13/viper"
 )
 
 func InitConfig() {
+	log := logger.New()
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
-	viper.ReadInConfig()
+	if err := viper.ReadInConfig(); err != nil {
+		log.Warn("if this message in docker conteiner, just skip it. But if it in local machine create .env file")
+	}
 }
 
 func GetString(key string) string {
