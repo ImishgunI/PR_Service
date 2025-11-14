b8301cbb007ce0de7f8222e09cd80715edaeab90
Michael <mvasilyev04@gmail.com>
2025-11-14 18:23:38 +0300

    update init function

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,20 +1,15 @@
 package config
 
 import (
-	"errors"
-
 	"github.com/spf13/viper"
 )
 
-func InitConfig() error {
+func InitConfig() {
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
-	if err := viper.ReadInConfig(); err != nil {
-		return errors.New("Reading .env file failed\n")
-	}
-	return nil
+	viper.ReadInConfig()
 }
 
 func GetString(key string) string {
