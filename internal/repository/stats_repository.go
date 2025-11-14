25be558d37a639106407122076ea15bf49a48c58
Michael <mvasilyev04@gmail.com>
2025-11-15 00:51:18 +0300

    create function for get reviewers from sql

---
+++ internal/repository/stats_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"PullRequestService/internal/db"
+	"PullRequestService/internal/models"
+	"context"
+)
+
+type StatsRepository interface {
+	GetReviewerStats(ctx context.Context) ([]models.ReviewersStats, error)
+}
+
+type Statistic struct {
+	db *db.DataBase
+}
+
+func NewStatisticRepository(db *db.DataBase) *Statistic {
+	return &Statistic{db: db}
+}
+
+func (s *Statistic) GetReviewerStats(ctx context.Context) ([]models.ReviewersStats, error) {
+	rows, err := s.db.Db.Query(ctx, `
+		SELECT u.user_id, u.username, COUNT(pr.reviewer_id) AS assigned_count
+        FROM users u
+        LEFT JOIN pr_reviewers pr ON u.user_id = pr.reviewer_id
+        GROUP BY u.user_id, u.username
+        ORDER BY assigned_count DESC;
+		`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	result := []models.ReviewersStats{}
+	for rows.Next() {
+		var s models.ReviewersStats
+		if err := rows.Scan(&s.UserID, &s.Username, &s.AssignedCount); err != nil {
+			return nil, err
+		}
+		result = append(result, s)
+	}
+	return result, nil
+}
